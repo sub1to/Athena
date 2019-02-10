034033388e6e41c83b1cf6f6ecbaf70122edf6a3
Charlotte Dunois <github@charuru.moe>
2019-02-10 21:39:34 +0100

    Set connect promise to null regardless

--- src/AthenaCache.php
+++ src/AthenaCache.php
@@ -84,13 +84,14 @@ class AthenaCache implements \CharlotteDunois\Events\EventEmitterInterface, Cach
         if(!$this->connectPromise) {
             $factory = new \Clue\React\Redis\Factory($this->loop, ($this->options['connector'] ?? null));
             $this->connectPromise = $factory->createClient((!empty($this->options['address']) ? $this->options['address'] : 'redis://127.0.0.1:6379'))->then(function (\Clue\React\Redis\Client $client) {
+                $this->connectPromise = null;
+                
                 if($this->destroyed) {
                     $client->close();
                     return;
                 }
                 
                 $this->redis = $client;
-                $this->connectPromise = null;
                 
                 $this->redis->on('error', function ($error) {
                     $this->emit('error', $error);
